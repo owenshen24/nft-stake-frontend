ab248a26811f6dea12eedc3b4f85b16afd85a489
Owen <owenshen24@gmail.com>
2020-10-09 11:35:24 -0700

    New 8bit gems

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -10,10 +10,12 @@ export class ConstantsService {
   YFB_ADDRESS = '0x89ee58af4871b474c30001982c3d7439c933c838';
   STAKER_ADDRESS = '0xC5802bCf619301Bdc6B1B9EDA4B6fd26e63339aF';
 
+  // NOTE: we're passing in the current block as endBlock if the gem is still staked
   mapGem(amount: BigNumber, startBlock: BigNumber, endBlock: BigNumber, isStaked: boolean) {
     let id = 1;
     let ext = ".gif";
     let folder = "/";
+    
     if (! isStaked) {
       ext = ".png";
       folder = "/dead"
@@ -30,10 +32,15 @@ export class ConstantsService {
     if (amount.isGreaterThan(new BigNumber(1000))) {
       id = 5;
     }
+
     if (startBlock.isLessThanOrEqualTo(new BigNumber(10993000))) {
       folder = "/genesis";
       ext = ".gif";
     }
+    else if (isStaked && endBlock.minus(startBlock).isGreaterThan(new BigNumber(10000))) {
+      folder = "/8bit";
+    }
+
     return "https://yflambda.herokuapp.com/images" + folder + "/gem-" + id + ext;
   }
 }
